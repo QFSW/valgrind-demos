32e60973ee9e7ba6965f7e8b0de88a9ff4b86b29
Yusuf Ismail <yusuf@qfsw.co.uk>
2021-02-26 15:15:46 +0000

    Fix

--- fn/leak-mmap.cpp
+++ fn/leak-mmap.cpp
@@ -3,7 +3,7 @@
 int main()
 {
     int bytes = 10000;
-    void* mem = mmap(0, 10000, PROT_READ|PROT_WRITE|PROT_EXEC, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);
+    void* mem = mmap(0, bytes, PROT_READ|PROT_WRITE|PROT_EXEC, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);
 
     return 0;
 }
